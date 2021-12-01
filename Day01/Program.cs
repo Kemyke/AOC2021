2653b6c6a1feec9ee385813bf19385f2939cab4f
Attila Kemeny <kemyyy@gmail.com>
2021-12-01 22:21:15 +0100

    day1

---
+++ Day01/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Day01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var depths = File.ReadAllLines("input.txt").Select(t => int.Parse(t));
+            var part1 = depths.Skip(1).Zip(depths).Select(t=> t.First - t.Second).Where(d=>d>0).Count();
+
+            var aggDepths = depths.Skip(2).Zip(depths.Skip(1).Zip(depths)).Select(d => d.First + d.Second.First + d.Second.Second);
+            var part2 = aggDepths.Skip(1).Zip(aggDepths).Select(t => t.First - t.Second).Where(d => d > 0).Count();
+        }
+    }
+}
